d06114b8baff11640d97c9e61ec761b47d8a85ed
nyugoh <nyugoh@gmail.com>
2017-05-14 00:20:36 +0300

    Express basics: routes, forms, view engines, scaffolding

---
+++ app.js
@@ -0,0 +1,67 @@
+//Import all the modules required
+var express = require('express'),
+    bodyParser = require('body-parser');
+
+var urlencodeParser = bodyParser.urlencoded({extended: false});
+
+// Create a new app
+var app = express();
+
+//Set the listening port
+app.set('port', process.env.PORT || 8000);
+//Set the render/ View engine --ejs
+app.set('view engine', 'ejs');
+
+//Set the router for static files
+app.use('/public', express.static('./public'));
+
+app.get('/', function (req, res, next){
+    console.log("Incoming request ::");
+    console.log("URL    --->" + req.url);
+    console.log("Method --->" + req.method);
+    console.log("Query  --->" + req.query);
+    console.log('');
+    next();
+});
+
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.get('/home', function(req,res){
+    res.sendFile(__dirname + '/views/message.html');
+});
+
+app.get('/contact/:name/:dept', function(req, res){
+    res.render('contact', {name: req.params.name, dept : req.params.dept});
+});
+
+app.get('/about', function(req, res){
+    var data = {name:'Joe', age:22, hobby:['Hacking', 'Hiking', 'Swimming', 'Workout', 'Play chess']};
+    res.render('about', {data: data});
+});
+
+app.get('/login', function(req, res){
+        res.render('login');
+});
+
+app.post('/login', urlencodeParser, function(req, res){
+        if(!req.body) res.send(404);
+        if(req.body.username == 'Joe'){
+            res.render('login-good', {data: req.body});
+        }else{
+            res.render('login', {response:'Username or password is wrong.'});
+        }
+        console.log(req.body);
+});
+
+app.get('/signup', function(req, res){
+        res.render('signup');
+});
+
+
+//Start the app
+app.listen(app.get('port'), function(){
+    console.log('App is running on port ' + app.get('port'));
+});
